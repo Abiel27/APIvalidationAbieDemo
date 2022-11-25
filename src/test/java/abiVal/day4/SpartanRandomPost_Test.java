1d5fe5e4432f8d492f31d6995dba98a1197d4af9
Abiel27 <abielmenghistu@gmail.com>
2022-11-25 06:31:20 -0500

    add One random Data Spartans POJO. and ge one to verify , then use extract method with in hte same chain, to fetch data.

--- src/test/java/abiVal/day4/SpartanRandomPost_Test.java
+++ src/test/java/abiVal/day4/SpartanRandomPost_Test.java
@@ -4,6 +4,7 @@ import abiVal.POJO.Spartan;
 import abiVal.Test_Utilit.SpartanNoAuthtestBaseclass;
 import abiVal.spartan_Utile.SpartanUtil;
 import io.restassured.http.ContentType;
+import io.restassured.response.Response;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.*;
 
@@ -11,6 +12,7 @@ import java.util.Map;
 
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
 
 public class SpartanRandomPost_Test extends SpartanNoAuthtestBaseclass {
 
@@ -51,6 +53,48 @@ public class SpartanRandomPost_Test extends SpartanNoAuthtestBaseclass {
                 .body("data.name",is(randomPOJO.getName()))
                 .body("data.gender",is(randomPOJO.getGender()))
                ;
+
+
+    }
+    @DisplayName("POST /spartans then GET IT TO VERIFY")
+    @Test
+    public void testAddOneDataThenGetONeDataToVerify(){
+        /*
+        *Send post request, save the generated ID
+        *check statuse code is 2001
+        * send get request using the id saved from above
+        * check statuse code is 200
+         */
+        Spartan randomPOJO = SpartanUtil.getRandomSpartanPOJO();
+        Response response = given()
+                .log().all()
+                .contentType(ContentType.JSON)
+                .body(randomPOJO).
+        when()
+                .post("/spartans").prettyPeek();// looking to ID
+       int newID= response.path("data.id");
+        System.out.println(newID);
+//        int newID = response.jsonPath().getInt("data.id");
+        assertThat(response.statusCode(), is(201));
+
+    }
+    @DisplayName("POST /spartans then GET IT TO VERIFY 2 WITH EXTRACT METHOD")
+    @Test
+    public void testAddOneDataThenGetONeDataToVerifyDirectInChainMethod(){
+        // get a data with in the chain using extract method
+        Spartan randomPOJO = SpartanUtil.getRandomSpartanPOJO();
+        int newID = given()
+                .log().all()
+                .contentType(ContentType.JSON)
+                .body(randomPOJO).
+                when()
+                .post("/spartans").
+        then()
+                .log().all()
+                .statusCode(201)
+                .extract()
+                .path("data.id");
+        System.out.println("newID = " + newID);
 
 
     }
